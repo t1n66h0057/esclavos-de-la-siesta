32d339258c2c80d79b2a010fe644add6ba868384
Martin Lopez <martinlopez.ss066@gmail.com>
2020-10-04 17:49:55 -0300

    Agregue el archivo contacto.php y cambie algunos colores

---
+++ secciones/contacto.php
@@ -0,0 +1,20 @@
+<?php
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$mensaje = $_POST['mensaje'];
+$para = 'esclavosdelasiesta@gmail.com';
+$titulo = 'Mensaje desde la pagina';
+$header = 'From: ' . $email;
+$msjCorreo = "Nombre: $nombre\n E-Mail: $email\n Mensaje:\n $mensaje";
+  
+if ($_POST['submit']) {
+if (mail($para, $titulo, $msjCorreo, $header)) {
+echo "<script language='javascript'>
+alert('Mensaje enviado, muchas gracias.');
+window.location.href = 'http://esclavosdelasiesta.com.ar';
+</script>";
+} else {
+echo 'Falló el envio';
+}
+}
+?>
